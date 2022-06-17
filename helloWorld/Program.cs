d70770cdfc3f14cf61ad7bf4e400b0617df17c7f
lidiasalo <lidia.salo@gmail.com>
2022-06-17 17:07:11 +0200

    Agregar archivos de proyecto.

---
+++ helloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!!");
+            Console.ReadKey(); 
+        }
+    }
+}
